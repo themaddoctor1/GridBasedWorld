6576c7c3d691a67e0bbdfc8b01f7335c8ebe0255
Christopher Hittner <chrishittner@gmail.com>
2015-11-14 00:14:23 -0500

    Map
    I commented on the Map class, which I added in the previous commit.

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,24 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Map.java
+ * 
+ * Christopher Hittner (c) 2015
  */
 package map;
 
 /**
+ * A Map is a type of CellGrid that uses MapLocations instead of Cells.
  *
- * @author Christopher
+ * @author Christopher Hittner
  */
 public class Map extends CellGrid {
     
+    /**
+     * Creates a Map of specified dimensions.
+     * @param x Size on x-axis
+     * @param y Size on y-axis
+     * @param z Size on z-axis
+     */
     public Map(int x, int y, int z) {
         super(x, y, z);
     }
     
+    /**
+     * Creates a Map based of a series of locations.
+     * @param grid The set of locations.
+     */
     public Map(MapLocation[][][] grid) {
         super(grid);
     }
     
+    /**
+     * Creates a Map based on another Map.
+     * @param other The other Map.
+     */
     public Map(Map other) {
         super(other);
     }
